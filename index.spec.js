9aac25c365862d8196ab117e280884cee69ef5d1
Vadym Aminov <aminov.v.o@gmail.com>
2024-01-28 22:06:54 +0300

    added test sample

--- index.spec.js
+++ index.spec.js
@@ -1,7 +1,3 @@
-import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
-import App from './';
-
 describe('App', () => {
     test('button click event', () => {
         expect(true).toBe(true);
